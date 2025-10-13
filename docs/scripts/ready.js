2af7c0062bb9e4f418b59bed5113978da60aee5a
Tiago Alejo Herrador <tiago.alejo.h@hotmail.com>
2025-10-13 11:08:04 -0300

    Create ready.js

---
+++ docs/scripts/ready.js
@@ -0,0 +1,8 @@
+<script>
+  // Marca el body como "ready" cuando la página esté cargada
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", () => document.body.classList.add("ready"));
+  } else {
+    document.body.classList.add("ready");
+  }
+</script>
